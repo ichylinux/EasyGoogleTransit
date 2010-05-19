188523f3828c3432a2bed1789ae7b05e995d6bfd
ichy <ichylinux@gmail.com>
2010-05-19 23:08:36 +0900

    初回コミット

---
+++ src/jp/co/hybitz/googletransit/TransitSearcherFactory.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (C) 2010 Hybitz.co.ltd
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ */
+package jp.co.hybitz.googletransit;
+
+import jp.co.hybitz.googletransit.searcher.MobilePullSearcher20100517;
+
+import org.xmlpull.v1.XmlPullParser;
+
+/**
+ * @author ichy <ichylinux@gmail.com>
+ */
+public class TransitSearcherFactory {
+
+    public static TransitSearcher createMobilePullSearcher(XmlPullParser parser) {
+        return new MobilePullSearcher20100517(parser);
+    }
+}
---
+++ src/jp/co/hybitz/googletransit/searcher/MobilePullSearcher20100517.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2010 Hybitz.co.ltd
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ */
+package jp.co.hybitz.googletransit.searcher;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import jp.co.hybitz.googletransit.TransitSearchException;
+import jp.co.hybitz.googletransit.TransitSearcher;
+import jp.co.hybitz.googletransit.model.TransitQuery;
+import jp.co.hybitz.googletransit.model.TransitResult;
+import jp.co.hybitz.googletransit.parser.TransitParser;
+import jp.co.hybitz.googletransit.parser.MobilePullParser20100517;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+/**
+ * @author ichy <ichylinux@gmail.com>
+ */
+public class MobilePullSearcher20100517 implements TransitSearcher {
+	private static final String GOOGLE = "http://www.google.co.jp/m/directions";
+	
+	private TransitParser transitParser;
+	
+	public MobilePullSearcher20100517(XmlPullParser parser) {
+	    this.transitParser = new MobilePullParser20100517(parser);
+	}
+	
+	public TransitResult search(TransitQuery query) throws TransitSearchException {
+		InputStream in = null;
+
+		try {
+			URL url = new URL(GOOGLE + createQueryString(query));
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.connect();
+			in = con.getInputStream();
+			return transitParser.parse(in);
+			
+		} catch (MalformedURLException e) {
+		    throw new TransitSearchException(e.getMessage(), e);
+		} catch (ProtocolException e) {
+            throw new TransitSearchException(e.getMessage(), e);
+		} catch (IOException e) {
+            throw new TransitSearchException(e.getMessage(), e);
+		} catch (XmlPullParserException e) {
+            throw new TransitSearchException(e.getMessage(), e);
+		} catch (Exception e) {
+            throw new TransitSearchException(e.getMessage(), e);
+		} finally {
+			if (in != null) { try {in.close();} catch (IOException e){} }
+		}
+	}
+	
+	private String createQueryString(TransitQuery query) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("?saddr=").append(URLEncoder.encode(query.getFrom()));
+		sb.append("&daddr=").append(URLEncoder.encode(query.getTo()));
+		sb.append("&time=&ttype=dep&ie=UTF8&f=d&dirmode=transit&num=3&dirflg=r");
+		return sb.toString();
+	}
+}
